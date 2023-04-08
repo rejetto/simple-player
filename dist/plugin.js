2f2f96077ecea5793228628a3f88daabed850e2c
Massimo Melina <a@rejetto.com>
2023-04-08 13:17:18 +0200

    use new api to read configuration

--- dist/plugin.js
+++ dist/plugin.js
@@ -6,5 +6,8 @@ exports.frontend_css = 'style.css'
 exports.frontend_js = 'main.js'
 
 exports.config = {
-    position: { frontend: true, type: 'select', options: ['left', 'right'] }
+    position: {
+        frontend: true, type: 'select', options: ['left', 'right'],
+        helperText: "This applies only to larger screens"
+    }
 }
--- dist/public/main.js
+++ dist/public/main.js
@@ -1,22 +1,24 @@
+(() => { // this wrapper avoids name clashing of outer variables and functions
+    const config = HFS.getPluginConfig()
+    console.log({ config }, HFS.getPluginKey())
+
 HFS.onEvent('fileMenu', ({ entry }) =>
     /\.(mp3|wav|aac|ogg|flac)$/i.test(entry.uri) &&
-    { label: "Play audio", icon: '🔈', onClick: () => play(entry) })
+        { label: "Play audio", icon: '🔈', onClick: () => play(entry) })
 
 function play(entry) {
     const root = document.getElementById('player')
     root.style.display = entry ? 'flex' : ''
     const audio = root.querySelector('audio')
-    if (entry) {
-        audio.src = entry.uri
-        audio.play()
-    }
-    else
-        audio.pause()
+    if (!entry)
+        return audio.pause()
+    audio.src = entry.uri
+    audio.play()
     root.querySelector('#player-title').innerText = entry.name
 }
 
 HFS.onEvent('afterMenuBar', () => `
-    <div id='player' style='${HFS.plugins['simple-player'].position}: 0'>
+    <div id='player' style='${config.position||'left'}: 0'>
         <audio controls></audio>
         <div>
             <button onclick="play()">✕</button>
@@ -24,3 +26,5 @@ HFS.onEvent('afterMenuBar', () => `
         </div>
     </div>
 `)
+
+})()
